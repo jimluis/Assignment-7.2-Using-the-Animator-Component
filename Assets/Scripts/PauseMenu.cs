bb49c8dc42fd9e31e51c0ca2f65aff4599120898
Luis Felipe Jimenez <jimluisf@gmail.com>
2022-03-06 13:48:42 -0500

    initial commit

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool gamePaused = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gamePaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        gamePaused = false;
+        Time.timeScale = 1f;
+        pauseMenuUI.SetActive(false);
+    }
+
+    void PauseGame()
+    {
+        gamePaused = true;
+        Time.timeScale = 0f;
+        pauseMenuUI.SetActive(true);
+    }
+
+    public void LoadGameMenu()
+    {
+        Debug.Log("PauseMenu.LoadGameMenu() SceneManager.LoadScene(0) ");
+        SceneManager.LoadScene(0);
+
+    }
+}
